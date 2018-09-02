732660b086e3e2ef1af74733aa2a35b0c4a53587
Eduardo Marques <edu.temarques@gmail.com>
2018-09-02 14:58:29 -0300

    Implementação do componente de tentativas

---
+++ src/app/try-status/try-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-try-status',
+  templateUrl: './try-status.component.html',
+  styleUrls: ['./try-status.component.scss']
+})
+export class TryStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
