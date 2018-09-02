29351afc27cc1736508d64d45efcff1e553ad3b6
Eduardo Marques <edu.temarques@gmail.com>
2018-09-02 14:58:56 -0300

    Implementação do componente de progresso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,30 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PanelComponent } from './panel/panel.component';
+import { TryStatusComponent } from './try-status/try-status.component';
+import { ProgressComponent } from './progress/progress.component';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PanelComponent
+    PanelComponent,
+    TryStatusComponent,
+    ProgressComponent
   ],
   imports: [
     BrowserModule,
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
+    MatProgressBarModule
     BrowserAnimationsModule,
     FormsModule
   ],
