ebfb5c095572d5ff61662bf1698f199d25f36e8f
Eduardo Marques <edu.temarques@gmail.com>
2018-09-02 20:45:10 -0300

    Correção de bug no appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatProgressBarModule
+    MatProgressBarModule,
     BrowserAnimationsModule,
     FormsModule
   ],
